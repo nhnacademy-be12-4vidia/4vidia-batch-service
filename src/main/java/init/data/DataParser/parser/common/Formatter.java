baac13a44f44cfec0ec59f78d65225f0b31bb510
Link <mexlove2@gmail.com>
2025-11-17 19:47:48 +0900

    네이버 API 데이터 날짜 형식 포맷터

---
+++ src/main/java/init/data/DataParser/parser/common/Formatter.java
@@ -0,0 +1,10 @@
+package init.data.DataParser.parser.common;
+
+import java.time.format.DateTimeFormatter;
+
+public class Formatter {
+
+  public static final DateTimeFormatter NAVER_DATE_FORMATTER = DateTimeFormatter.ofPattern(
+      "yyyyMMdd");
+
+}
