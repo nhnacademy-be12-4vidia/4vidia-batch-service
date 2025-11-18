674d2c6719106e0c2436374382b1a9a9571deea6
Link <mexlove2@gmail.com>
2025-11-18 19:15:54 +0900

    500개씩 처리하게 수정

--- src/main/java/init/data/DataParser/parser/Api/NaverApiParser.java
+++ src/main/java/init/data/DataParser/parser/Api/NaverApiParser.java
@@ -1,8 +1,8 @@
 package init.data.DataParser.parser.Api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import init.data.DataParser.DTO.NaverApiResponseDto;
-import init.data.DataParser.DTO.NaverParsingDto;
+import init.data.DataParser.DTO.Naver.NaverApiResponseDto;
+import init.data.DataParser.DTO.Naver.NaverParsingDto;
 import init.data.DataParser.entity.Book;
 import init.data.DataParser.entity.Publisher;
 import init.data.DataParser.parser.common.Formatter;
@@ -12,7 +12,6 @@ import jakarta.annotation.PostConstruct;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -20,7 +19,6 @@ import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -37,7 +35,6 @@ public class NaverApiParser {
 
   private final BookRepository bookRepository;
   private final PublisherRepository publisherRepository;
-  private final Environment env;
 
   @Value("${naver.api.client-id}")
   private String clientId;
@@ -47,55 +44,61 @@ public class NaverApiParser {
 
   public void searchAndSaveBooks(String keyword) {
 
+    final int display = 100;
+    final int totalPages = 5;
+
     try {
       log.info("------{} 관련 도서 검색 시작------", keyword);
-      log.info(env.toString());
       log.info(clientSecret);
 
       String encodedKeyword = URLEncoder.encode(keyword, StandardCharsets.UTF_8);
-      String apiUrl = "https://openapi.naver.com/v1/search/book.json?query=%s".formatted(encodedKeyword);
 
-      URL url = new URL(apiUrl);
-      HttpURLConnection con = (HttpURLConnection) url.openConnection();
-      con.setRequestMethod("GET");
-      con.setRequestProperty("X-Naver-Client-Id", clientId);
-      con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
+      for (int i = 0; i < totalPages; i++) {
+        int start = 1 + (i * display);
+        String apiUrl = "https://openapi.naver.com/v1/search/book.json?query=%s&display=%s&start=%s".formatted(encodedKeyword, display, start);
 
-      BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-      String line;
-      StringBuffer responseJson = new StringBuffer();
-      while ((line = br.readLine()) != null) {
-        responseJson.append(line);
-      }
-      br.close();
+        URL url = new URL(apiUrl);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("X-Naver-Client-Id", clientId);
+        con.setRequestProperty("X-Naver-Client-Secret", clientSecret);
 
-      ObjectMapper objectMapper = new ObjectMapper();
-      NaverApiResponseDto naverApiResponseDto = objectMapper.readValue(responseJson.toString(),
-          NaverApiResponseDto.class);
-
-      for (NaverParsingDto dto : naverApiResponseDto.getItems()) {
-        String publisherName = dto.getPublisher();
-        Publisher publisher;
-        if (publisherRepository.existsByName(publisherName)) {
-          publisher = publisherRepository.findByName(publisherName);
-        } else {
-          publisher = new Publisher();
-          publisher.setName(publisherName);
-          publisherRepository.save(publisher);
+        BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String line;
+        StringBuffer responseJson = new StringBuffer();
+        while ((line = br.readLine()) != null) {
+          responseJson.append(line);
         }
+        br.close();
 
-        Book book = Book.builder()
-            .title(dto.getTitle())
-            .priceStandard(parseLongFromString(dto.getPrice()))
-            .priceSales(parseLongFromString(dto.getDiscount()))
-            .publisher(publisher)
-            .imageUrl(dto.getImage())
-            .description(dto.getDescription())
-            .longIsbn(parseLongFromString(dto.getIsbn()))
-            .publishedDate(LocalDate.parse(dto.getPubdate(), Formatter.NAVER_DATE_FORMATTER))
-            .build();
+        ObjectMapper objectMapper = new ObjectMapper();
+        NaverApiResponseDto naverApiResponseDto = objectMapper.readValue(responseJson.toString(),
+            NaverApiResponseDto.class);
 
-        bookRepository.save(book);
+        for (NaverParsingDto dto : naverApiResponseDto.getItems()) {
+          String publisherName = dto.getPublisher();
+          Publisher publisher;
+          if (publisherRepository.existsByName(publisherName)) {
+            publisher = publisherRepository.findByName(publisherName);
+          } else {
+            publisher = new Publisher();
+            publisher.setName(publisherName);
+            publisherRepository.save(publisher);
+          }
+
+          Book book = Book.builder()
+              .title(dto.getTitle())
+              .priceStandard(parseLongFromString(dto.getPrice()))
+              .priceSales(parseLongFromString(dto.getDiscount()))
+              .publisher(publisher)
+              .imageUrl(dto.getImage())
+              .description(dto.getDescription())
+              .longIsbn(parseLongFromString(dto.getIsbn()))
+              .publishedDate(LocalDate.parse((StringUtils.hasText(dto.getPubdate()) ? dto.getPubdate() : "22000101"), Formatter.NAVER_DATE_FORMATTER))
+              .build();
+
+          bookRepository.save(book);
+        }
       }
     } catch (Exception e) {
       throw new RuntimeException(e);
