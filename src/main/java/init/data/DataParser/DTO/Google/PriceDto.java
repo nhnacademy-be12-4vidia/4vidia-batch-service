f34e08a4a4431ff3195e7a43057afa0246b4061e
Link <mexlove2@gmail.com>
2025-11-18 19:17:25 +0900

    구글 파싱용 DTO 생성

---
+++ src/main/java/init/data/DataParser/DTO/Google/PriceDto.java
@@ -0,0 +1,17 @@
+package init.data.DataParser.DTO.Google;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
+@NoArgsConstructor
+public class PriceDto {
+
+  @JsonProperty("amount")
+  private Long amount;
+}
---
+++ src/main/java/init/data/DataParser/DTO/Google/SaleInfoDto.java
@@ -0,0 +1,20 @@
+package init.data.DataParser.DTO.Google;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Getter
+@Setter
+@NoArgsConstructor
+public class SaleInfoDto {
+
+  @JsonProperty("listPrice")
+  private PriceDto listPrice;
+
+  @JsonProperty("retailPrice")
+  private PriceDto retailPrice;
+}
