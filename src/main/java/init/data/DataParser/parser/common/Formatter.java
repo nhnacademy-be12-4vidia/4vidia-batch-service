4bac33026539bd1f76b62149dc99746342345298
Link <mexlove2@gmail.com>
2025-11-19 19:23:47 +0900

    범용 날짜 포매터 추가

--- src/main/java/init/data/DataParser/parser/common/Formatter.java
+++ src/main/java/init/data/DataParser/parser/common/Formatter.java
@@ -7,4 +7,6 @@ public class Formatter {
     public static final DateTimeFormatter NAVER_DATE_FORMATTER = DateTimeFormatter.ofPattern(
         "yyyyMMdd");
 
+    public static final DateTimeFormatter DEFAULT_DATE_FORMATTER = DateTimeFormatter.ofPattern(
+        "yyyy-MM-dd");
 }
