104dc8dc7ce947a1bad13e322593eb80246bdcc9
Link <mexlove2@gmail.com>
2025-11-19 19:20:33 +0900

    서식 변경

--- src/main/java/init/data/DataParser/entity/Book.java
+++ src/main/java/init/data/DataParser/entity/Book.java
@@ -27,70 +27,79 @@ public class Book extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    @Column(name = "ISBN_13", length = 13)
+
+    @Column(name = "ISBN", length = 13)
     @Setter
-    private Long longIsbn;
-    @Column(name = "ISBN_10", length = 10)
-    @Setter
-    private Long shortIsbn;
-    @Column(name = "제목", nullable = false)
+    private String isbn;
+
+    @Column(name = "제목", nullable = false, length = 1000)
     @Setter
     private String title;
+
     @Column(name = "부제목")
     @Setter
     private String subtitle;
+
     @Column(name = "목차", columnDefinition = "TEXT")
     @Setter
     private String index;
+
     @Column(name = "설명", columnDefinition = "TEXT")
     @Setter
     private String description;
-    @Column(name = "이미지url", length = 500)
-    @Setter
-    private String imageUrl;
+
+//    @Column(name = "이미지url", length = 500)
+//    @Setter
+//    private String imageUrl;
+
     @ManyToOne
     @Setter
     private Publisher publisher;
+
     @Column(name = "출판일시")
     @Setter
     private LocalDate publishedDate;
+
     @Column(name = "페이지수")
     @Setter
     private Integer pageCount;
-    @Column(name = "언어")
+
+    @Column(name = "언어", length = 10)
     @Setter
     private String language;
+
     @Column(name = "정가")
     @Setter
-    private Long priceStandard;
+    private Integer priceStandard;
+
     @Column(name = "판매가")
     @Setter
-    private Long priceSales;
+    private Integer priceSales;
+
     @Column(name = "재고", columnDefinition = "INT DEFAULT 0")
     @Setter
     private Integer stock;
+
     @Column(name = "재고상태")
     @Setter
     @Enumerated(value = EnumType.STRING)
     private StockStatus stockStatus = StockStatus.OUT_OF_STOCK;
+
     @Column(name = "포장가능여부")
     @Setter
     private Boolean packagingAvailable = false;
-    @Column(name = "평가평균", precision = 3, scale = 2)
-    @Setter
-    private BigDecimal ratingAverage;
+
     @Column(name = "총권수", columnDefinition = "INT DEFAULT 0")
     @Setter
     private Integer volumeNumber;
 
     @Builder
-    public Book(Long longIsbn, String title, String description, String imageUrl,
+    public Book(String isbn, String title, String description,
         Publisher publisher,
-        LocalDate publishedDate, Long priceStandard, Long priceSales) {
-        this.longIsbn = longIsbn;
+        LocalDate publishedDate, Integer priceStandard, Integer priceSales) {
+        this.isbn = isbn;
         this.title = title;
         this.description = description;
-        this.imageUrl = imageUrl;
         this.publisher = publisher;
         this.publishedDate = publishedDate;
         this.priceSales = priceSales;
