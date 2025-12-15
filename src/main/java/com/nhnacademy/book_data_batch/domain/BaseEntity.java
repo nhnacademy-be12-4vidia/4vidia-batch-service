8f4cac1892fd2e2cd1cbba7cb6284a8a3e559403
Quasimorphism <bak575755@gmail.com>
2025-12-15 15:17:56 +0900

    fix: BaseEntity에 unused import 삭제 -

--- src/main/java/com/nhnacademy/book_data_batch/domain/BaseEntity.java
+++ src/main/java/com/nhnacademy/book_data_batch/domain/BaseEntity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
